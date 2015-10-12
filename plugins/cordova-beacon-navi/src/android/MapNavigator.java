d7ab320368d9e3e567bda13f03440a08ee23d61a
Yin Xiaoyu <172278255@qq.com>
2015-10-12 15:31:59 +0800

    indoor navigation android plugin dev

--- plugins/cordova-beacon-navi/src/android/MapNavigator.java
+++ plugins/cordova-beacon-navi/src/android/MapNavigator.java
@@ -1,5 +1,9 @@
 package com.cloudnapps.plugins.map;
 
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
 import org.apache.cordova.CordovaWebView;
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CordovaInterface;
@@ -11,6 +15,14 @@ import org.json.JSONObject;
 
 public class MapNavigator extends CordovaPlugin {
 
+    private Context mContext;
+    private CordovaInterface mCordova;
+
+    protected String mCityId;
+    protected String mBuildingId;
+    protected String mUserId;
+    protected String mLicense;
+    protected String mBeaconUUID;
     /**
      * Constructor.
      */
@@ -27,6 +39,17 @@ public class MapNavigator extends CordovaPlugin {
      */
     public void initialize(CordovaInterface cordova, CordovaWebView webView) {
         super.initialize(cordova, webView);
+        mCordova = cordova;
+        mContext = cordova.getActivity();
+
+        Intent intent = cordova.getActivity().getIntent();
+        if (intent != null) {
+            mCityId = this.preferences.getString("cityID", "");
+            mBuildingId = this.preferences.getString("buildingID", "");
+            mUserId = this.preferences.getString("userID", "");
+            mLicense = this.preferences.getString("licenseID", "");
+            mBeaconUUID = this.preferences.getString("beaconUUID", "");
+        }
     }
 
     /**
@@ -38,10 +61,20 @@ public class MapNavigator extends CordovaPlugin {
      * @return                  True if the action was valid, false if not.
      */
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-        if (action.equals("")) {
-            
-        }
-        else {
+        if ("showShopMap".equals(action) || "showMapNavigator".equals(action)) {
+            Intent intent = new Intent(mContext, MapActivity.class);
+
+            intent.putExtra("cityID", mCityId);
+            intent.putExtra("buildingID", mBuildingId);
+            intent.putExtra("userID", mUserId);
+            intent.putExtra("licenseID", mLicense);
+            intent.putExtra("beaconUUID", mBeaconUUID);
+
+            intent.putExtra("poiID", args.getString(0));
+            intent.putExtra("floorNum", args.getString(1));
+
+            mContext.startActivity(intent);
+        } else {
             return false;
         }
         return true;
