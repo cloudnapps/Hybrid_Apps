30f53e98995f3fc6d817f77213801213fbb1cc89
Yin Xiaoyu <172278255@qq.com>
2015-10-20 13:17:04 +0800

    change server address

--- ssas/www/js/components/components.js
+++ ssas/www/js/components/components.js
@@ -1,12 +1,12 @@
 (function(){
   angular.module('components', ['starter.services'])
     .constant("apiEndpoint", {url: "/m"})
-    // .constant("apiEndpoint", {url:"http://bbc.jooau.com/zhongshihua/index.php/m"})
+//    .constant("apiEndpoint", {url:"http://zdf.jooau.com/index.php/m"})
     .constant("tabIndex", {
     	home: 0,
     	shop: 1,
     	cart: 2,
     	member: 3
-    })
 
+    });
 })();
