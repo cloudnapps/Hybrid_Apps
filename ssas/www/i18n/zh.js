b85eeef0729b7bd06612120b2f1067e0cf95c1e9
minxianlong <66794531@qq.com>
2015-11-05 14:08:09 +0800

    issue 28, 男女翻译的问题

--- ssas/www/i18n/zh.js
+++ ssas/www/i18n/zh.js
@@ -1,110 +1,112 @@
 var translations_zh = {
-    search: 'Search',
-    cancel: 'Cancel',
-    about: 'About',
-    books: 'Books',
-    media: 'Media',
-    places: 'Places',
-    options: 'Options',
-    credits: 'Help & Credits',
-    language: 'Language',
-    lang_en: 'english',
-    lang_es: 'spanish',
-    lang_fr: 'french',
-    lang_it: 'italian',
+  search: 'Search',
+  cancel: 'Cancel',
+  about: 'About',
+  books: 'Books',
+  media: 'Media',
+  places: 'Places',
+  options: 'Options',
+  credits: 'Help & Credits',
+  language: 'Language',
+  lang_en: 'english',
+  lang_es: 'spanish',
+  lang_fr: 'french',
+  lang_it: 'italian',
 
-    //common
-    save: '保存',
-    close: '关闭',
-    remove: '删除',
+  //common
+  save: '保存',
+  close: '关闭',
+  remove: '删除',
 
-    //main tabs
-    home: '商城',
-    cart: '购物车',
-    shop: '分类',
-    my: '我',
+  //main tabs
+  home: '商城',
+  cart: '购物车',
+  shop: '分类',
+  my: '我',
 
-    // business
-    direct_mail: '香港直邮',
-    bond: '保税仓',
-    bond_place: '(深圳前海)',
+  // business
+  direct_mail: '香港直邮',
+  bond: '保税仓',
+  bond_place: '(深圳前海)',
 
-    //login
-    login_title: '登录',
-    register_title: '新用户注册',
-    retrieve_password: '找回密码',
+  //login
+  login_title: '登录',
+  register_title: '新用户注册',
+  retrieve_password: '找回密码',
 
-    //seller
-    seller_title:'商户列表',
-    seller_detail: '商戸详情',
+  //seller
+  seller_title: '商户列表',
+  seller_detail: '商戸详情',
 
-    //signin
-    signin_title: '签个到',
+  //signin
+  signin_title: '签个到',
 
-    //order
-    ord_alltitle:'全部',
-    ord_nopaytitle: '待付款',
-    ord_noshiptitle: '待发货',
-    ord_shippedtitle: '待收货',
-    ord_nocommenttitle: '待评价',
-    ord_returntitle: '售后',
+  //order
+  ord_alltitle: '全部',
+  ord_nopaytitle: '待付款',
+  ord_noshiptitle: '待发货',
+  ord_shippedtitle: '待收货',
+  ord_nocommenttitle: '待评价',
+  ord_returntitle: '售后',
 
-    comment_title: '订单评论',
+  comment_title: '订单评论',
 
-    //favorite
-    fav_title: '我的收藏',
-    fav_goods: '商品',
-    fav_sellers: '店铺',
+  //favorite
+  fav_title: '我的收藏',
+  fav_goods: '商品',
+  fav_sellers: '店铺',
 
-    good_category: '商品分类',
-    rec_title: '地址管理',
-    product_detail: '商品详情',
-    mkt_price: '市场价格',
+  good_category: '商品分类',
+  rec_title: '地址管理',
+  product_detail: '商品详情',
+  mkt_price: '市场价格',
 
-    comment: '评论',
+  comment: '评论',
 
-    //receiver & idcard
-    rec_title: '地址管理',
-    rec_addbtn: '添加新地址',
-    rec_addtitle: '新建收货地址',
-    idcard_title: '身份证管理',
-    idcard_addbtn: '添加身份证',
-    idcard_addtitle: '新建身份证',
+  //receiver & idcard
+  rec_title: '地址管理',
+  rec_addbtn: '添加新地址',
+  rec_addtitle: '新建收货地址',
+  idcard_title: '身份证管理',
+  idcard_addbtn: '添加身份证',
+  idcard_addtitle: '新建身份证',
 
-    //point & gold
-    point_title: '我的积分',
-    point_list: '积分记录',
-    gold_title: '我的金币',
-    gold_list: '金币记录',
+  //point & gold
+  point_title: '我的积分',
+  point_list: '积分记录',
+  gold_title: '我的金币',
+  gold_list: '金币记录',
 
-    //setting
-    settng_title: '个人资料',
+  //setting
+  settng_title: '个人资料',
+  gentle: '男',
+  lady: '女',
 
-    //return & complaint
-    return_title: '售后',
-    return_addbtn: '提交申请',
-    return_newbtn: '我要售后',
-    complaint_title: '投诉与建议',
-    complaint_addbtn: '提交',
-    complaint_newbtn: '新建投诉与建议',
-    submit_comment: '发表评论',
+  //return & complaint
+  return_title: '售后',
+  return_addbtn: '提交申请',
+  return_newbtn: '我要售后',
+  complaint_title: '投诉与建议',
+  complaint_addbtn: '提交',
+  complaint_newbtn: '新建投诉与建议',
+  submit_comment: '发表评论',
 
-    //good
-    good_category: '商品分类',
-    good_fav: '收藏商品',
+  //good
+  good_category: '商品分类',
+  good_fav: '收藏商品',
 
-    product_cart: '加入购物车',
-    product_goto_shop: '进入店铺',
-    product_fav_shop: '收藏店铺',
+  product_cart: '加入购物车',
+  product_goto_shop: '进入店铺',
+  product_fav_shop: '收藏店铺',
 
-    //cart
-    cart_empty: '购物车空空如也～～～',
-    submit_order: '提交订单',
-    cart_checkout: '结算',
-    cart_order_confirm: '订单确认',
-    pay_now: '立刻支付',
-    shipping: '配送方式',
+  //cart
+  cart_empty: '购物车空空如也～～～',
+  submit_order: '提交订单',
+  cart_checkout: '结算',
+  cart_order_confirm: '订单确认',
+  pay_now: '立刻支付',
+  shipping: '配送方式',
 
-    cart_payment: '选择支付方式',
-    order_payed: '订单支付成功'
+  cart_payment: '选择支付方式',
+  order_payed: '订单支付成功'
 };
