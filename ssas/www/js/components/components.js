5ac57e8e1ea35a35081a353b345771510d8bc948
Tony <igitinode@gmail.com>
2015-11-23 16:03:02 +0800

    fx the style for require

--- ssas/www/js/components/components.js
+++ ssas/www/js/components/components.js
@@ -1,6 +1,6 @@
 (function(){
   angular.module('components', ['starter.services'])
-    //.constant('apiEndpoint', {url: '/m'})
+    // .constant('apiEndpoint', {url: '/m'})
     .constant('apiEndpoint', {url:'http://www.ctfhoko.com/index.php/m'})
     .constant('tabIndex', {
     	home: 0,
